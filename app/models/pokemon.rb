ebdbdaa65563096fb9d90cd3105d7b7edc401c77
MMcClure11 <mcclme02@luther.edu>
2021-03-08 17:51:13 -0600

    Add Pokemon validations

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,4 +1,7 @@
 class Pokemon < ApplicationRecord
     has_many :pokedexes
     has_many :users, through: :pokedexes
+
+    validates :name, :p_id, presence: true
+    validates :name, :p_id, uniqueness: true
 end
