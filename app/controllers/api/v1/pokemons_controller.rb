55efcd4444a75bceee701fe0e874afa32e674272
MMcClure11 <mcclme02@luther.edu>
2021-03-08 17:19:14 -0600

    Add name spacing to controllers and routes to move controllers into api/v1 folder

--- app/controllers/api/v1/pokemons_controller.rb
+++ app/controllers/api/v1/pokemons_controller.rb
@@ -1,4 +1,4 @@
-class PokemonsController < ApplicationController
+class Api::V1::PokemonsController < ApplicationController
 
   def index
     render json: Pokemon.all, status: 200
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :pokemons
-  resources :users
+
+  namespace :api do 
+    namespace :v1 do 
+      resources :pokemons
+      resources :users
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
