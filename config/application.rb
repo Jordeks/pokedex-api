e61c20d63ab846a824439fa471b12213e7b7e51f
MMcClure11 <mcclme02@luther.edu>
2021-03-08 19:11:03 -0600

    Add cookie and session middleware to application.rb

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,7 @@ module PokedexApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore, key: '_cookie_name'
   end
 end
