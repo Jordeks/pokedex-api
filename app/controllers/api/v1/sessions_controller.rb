d9c70cd252832e30d5a95c96d8c1e0b06c9a40c0
MMcClure11 <mcclme02@luther.edu>
2021-03-08 20:10:50 -0600

    Add get_current_user action to SessionsController

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -13,4 +13,14 @@ class Api::V1::SessionsController < ApplicationController
     end
   end
 
+  def get_current_user
+    if logged_in?
+      render json: current_user, status: 200
+    else
+      render json: {
+        error: "No one logged in"
+      }
+    end
+  end
+
 end
