a99ec51d98833f6fd2e5b522c2890ca636b9587f
MMcClure11 <mcclme02@luther.edu>
2021-03-08 20:26:39 -0600

    Add create action to UsersController for signup with private action of user_params

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,22 @@
 class Api::V1::UsersController < ApplicationController
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      render json: user, status: 200
+    else
+      response = {
+        error: user.errors.full_messages.to_sentence
+      }
+      render json: response, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:username, :password)
+  end
+
 end
