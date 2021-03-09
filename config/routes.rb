97f9ba578946f025bc7425d2985b36db004df102
MMcClure11 <mcclme02@luther.edu>
2021-03-08 19:40:26 -0600

    Add SessionsController, change sessions and users routes to fix namespacing issue with api/v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
 
+  post "/api/v1/login", to: "api/v1/sessions#create"
+  post "/api/v1/signup", to: "api/v1/users#create"
+  delete "/api/v1/logout", to: "api/v1/sessions#destroy"
+  get "/api/v1/get_current_user", to: "api/v1/sessions#get_current_user"
+
   namespace :api do 
     namespace :v1 do 
       resources :pokemons
       resources :users
-
-      post "/login", to: "/sessions#create"
-      post "/signup", to: "/users#create"
-      delete "/logout", to: "/sessions#destroy"
-      get "/get_current_user", to: "/sessions#get_current_user"
-
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
