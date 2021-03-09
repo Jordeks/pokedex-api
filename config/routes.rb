42d7acd6133721b1b464cd98860967ed7bdb6418
MMcClure11 <mcclme02@luther.edu>
2021-03-08 19:30:34 -0600

    Add login, signup, logout, and get_current_user routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,12 @@ Rails.application.routes.draw do
     namespace :v1 do 
       resources :pokemons
       resources :users
+
+      post "/login", to: "/sessions#create"
+      post "/signup", to: "/users#create"
+      delete "/logout", to: "/sessions#destroy"
+      get "/get_current_user", to: "/sessions#get_current_user"
+
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
