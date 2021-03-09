0a736b5bf5f2247d0f55e1df7e51d15397ca38b7
MMcClure11 <mcclme02@luther.edu>
2021-03-08 19:58:15 -0600

    Add login with create action in SessionsController

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,2 +1,16 @@
 class Api::V1::SessionsController < ApplicationController
+
+  def create 
+    user = User.find_by(username: params[:session][:username])
+
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      render json: user, status: 200
+    else
+      render json: {
+        error: "Invalid Credentials"
+      }
+    end
+  end
+
 end
