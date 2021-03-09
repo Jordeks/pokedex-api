4cf0962ba55e85a8753e4f4f8600a0f2b423e538
MMcClure11 <mcclme02@luther.edu>
2021-03-08 20:33:11 -0600

    Update PokemonsController index action to get the pokemons for the current user

--- app/controllers/api/v1/pokemons_controller.rb
+++ app/controllers/api/v1/pokemons_controller.rb
@@ -1,7 +1,8 @@
 class Api::V1::PokemonsController < ApplicationController
 
   def index
-    render json: Pokemon.all, status: 200
+    pokemons = current_user.pokemons
+    render json: pokemons, status: 200
   end
   
 end
